b1bbf332395be3e425be9be50778a1d314312604
SI-ChrisL <clacy1113@gmail.com>
2025-11-15 11:42:57 -0600

    Add get_sorted_dictionary function and update main.py to utilize it

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, get_sorted_dictionary
 
 def main():
         book_path = "books/frankenstein.txt"
         text = get_book_text(book_path)
         num_words = get_num_words(text)
         num_chars = get_num_characters(text)
-        print(f"Found {num_words} total words")
-        print(num_chars)
+        # print(f"Found {num_words} total words")
+        # print(num_chars)
+        output = get_sorted_dictionary(num_chars)
+        print(output)
 
 def get_book_text(path):
         with open(path) as file:
--- stats.py
+++ stats.py
@@ -14,3 +14,11 @@ def get_num_characters(text):
                 char_counts[lowered_char] = 1
 
     return char_counts
+
+def get_sorted_dictionary(counts):
+    sorted_counts = []
+    for key in counts:
+        key_value = counts[key]
+        sub_directory = {"char": key, "num": key_value}
+        sorted_counts.append(sub_directory)
+    return sorted_counts
